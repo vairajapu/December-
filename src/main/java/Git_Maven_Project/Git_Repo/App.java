049bfaa250ffd892512b4653cf8e6586c5e4715c
Vaishali <vaishalibakale@gmail.com>
2020-12-04 13:24:15 +0530

    Initial commit

---
+++ src/main/java/Git_Maven_Project/Git_Repo/App.java
@@ -0,0 +1,13 @@
+package Git_Maven_Project.Git_Repo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
